e2aa53bb1c48f5d349cece5f568eaa36517d5b92
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-07-08 23:36:12 -0300

    Models - ManyToManyField

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Cliente, Telefone, CPF
+from .models import Empregado, Telefone, CPF, Departamento
 
 # Register your models here.
 
-admin.site.register(Cliente)
+admin.site.register(Empregado)
 admin.site.register(Telefone)
 admin.site.register(CPF)
+admin.site.register(Departamento)
--- clientes/models.py
+++ clientes/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
+class Departamento(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
 class CPF(models.Model):
     numero = models.CharField(max_length=14)
     data_exp = models.DateTimeField(auto_now=False)
@@ -10,13 +18,14 @@ class CPF(models.Model):
         return self.numero
 
 
-class Cliente(models.Model):
+class Empregado(models.Model):
     nome = models.CharField(max_length=100, null=False)
     endereco = models.CharField(max_length=200, blank=False, null=False)
     Salario = models.DecimalField(max_digits=10, decimal_places=2)
     idade = models.IntegerField()
     email = models.EmailField()
     cpf = models.OneToOneField(CPF, on_delete=models.CASCADE, null=True, blank=True)
+    departamento = models.ManyToManyField(Departamento, blank=True)
 
     def __str__(self):
         return self.nome
@@ -25,7 +34,7 @@ class Cliente(models.Model):
 class Telefone(models.Model):
     numero = models.CharField(max_length=20)
     descricao = models.CharField(max_length=80)
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    empregado = models.ForeignKey(Empregado, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.descricao + " - " + self.numero
