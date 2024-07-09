ca3ea22d96da72ad10c0c5630a1fc13013453ceb
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-07-08 22:43:14 -0300

    ModelsOneToOneField

--- clientes/models.py
+++ clientes/models.py
@@ -2,12 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+class CPF(models.Model):
+    numero = models.CharField(max_length=14)
+    data_exp = models.DateTimeField(auto_now=False)
+
+    def __str__(self):
+        return self.numero
+
+
 class Cliente(models.Model):
     nome = models.CharField(max_length=100, null=False)
     endereco = models.CharField(max_length=200, blank=False, null=False)
     Salario = models.DecimalField(max_digits=10, decimal_places=2)
     idade = models.IntegerField()
     email = models.EmailField()
+    cpf = models.OneToOneField(CPF, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.nome
@@ -20,4 +29,6 @@ class Telefone(models.Model):
 
     def __str__(self):
         return self.descricao + " - " + self.numero
-        
+
+
+
