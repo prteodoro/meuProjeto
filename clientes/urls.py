23522b12743fbfa1f0c3f64b5cf667d356defc20
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-07-12 20:17:44 -0300

    Templates Fundamentos/Condicionais/Loops

--- clientes/urls.py
+++ clientes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.home, name="home"),
     path("clientes/", views.clientes, name="clientes"),
     path("cliente/<int:pk>", views.cliente_detalhe, name="cliente_detalhe"),
     path("cliente/<str:nome>", views.cliente_por_nome, name="cliente_por_nome"),
--- clientes/views.py
+++ clientes/views.py
@@ -3,6 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
+def home(request):
+    sexo = "m"
+    nome = "Pedro"
+    lista = [
+        {"nome": "Pedro", "sexo": "m"},
+        {"nome": "Maria", "sexo": "f"},
+        {"nome": "Joaquina", "sexo": "F"},
+        {"nome": "Joao", "sexo": "M"}
+    ]
+    data = {"sexo": sexo, "nome": nome, "lista": lista}
+    return render(request, "index.html", data)
+
 def index(request):
     return HttpResponse("Olá Mundo")
 
